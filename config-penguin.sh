25d8fc0568ecb13f91a8a3287458d6570f1d312b
Mischa Radenovic <miroslav.radenovic@gmail.com>
2020-03-04 18:31:43 -0500

    refactor: Consolidate functionality

--- config-penguin.sh
+++ config-penguin.sh
@@ -3,6 +3,13 @@
 sudo apt update
 sudo apt dist-upgrade -y
 
+# add vs code repo
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+
+# install vscode
+sudo apt update && sudo apt install code -y
+
 # install nvm and expo
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 nvm install --lts
@@ -19,33 +26,17 @@ sudo apt update && sudo apt install --no-install-recommends yarn -y
 sudo apt install simple-scan -y
 sudo echo -e "# Add ET-2550 scanner\nnet 192.168.0.12\n" | sudo tee -a /etc/sane.d/epson2.conf
 
-# install nano
+# install nano and config git
 sudo apt install nano -y
- 
-# sets up Git with your name
+
 git config --global user.name "Mischa Radenovic"
- 
-# sets up Git with your email
 git config --global user.email "miroslav.radenovic@gmail.com"
- 
-# makes sure that Git output is colored
 git config --global color.ui auto
- 
-# displays the original state in a conflict
 git config --global merge.conflictstyle diff3
- 
-# sets up Git editor
 git config --global core.editor "nano"
  
 git config --list
 
+# config git completion
 curl -sL https://raw.githubusercontent.com/mradenovic/penguin-dev-config/master/config-git-completion.sh | bash
  
-
-# add vs code repo
-curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
-
-# install vscode
-sudo apt update && sudo apt install code -y
-
