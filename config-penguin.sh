9b15cf04195896c92d0d8e6fabb4337d2e1bd961
Mischa Radenovic <miroslav.radenovic@gmail.com>
2020-03-08 22:53:13 -0400

    fix: Load nvm after install

--- config-penguin.sh
+++ config-penguin.sh
@@ -12,7 +12,11 @@ sudo apt update && sudo apt install code -y
 
 # install nvm and expo
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
-bash
+
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 nvm install --lts
 npm install -g expo-cli
 
