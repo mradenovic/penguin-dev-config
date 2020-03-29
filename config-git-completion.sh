d8472f4c5de424a159c14eaf1344f6ebcd413844
Mischa Radenovic <mradenovic@users.noreply.github.com>
2020-03-29 15:32:14 -0400

    fix: Update file name

--- config-git-completion.sh
+++ config-git-completion.sh
@@ -5,7 +5,7 @@ CONFIG_DIR=~/.git-terminal-config
 rm -rf ${CONFIG_DIR}
 mkdir ${CONFIG_DIR}
 curl -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ${CONFIG_DIR}/git-completion.bash
-curl -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-promp.sh -o ${CONFIG_DIR}/git-prompt.sh
+curl -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ${CONFIG_DIR}/git-prompt.sh
 
 # append script to existing .bashrc
 curl -sSL https://raw.githubusercontent.com/mradenovic/penguin-dev-config/master/.bashrc | tee -a ~/.bashrc
